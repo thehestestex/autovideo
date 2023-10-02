4dc25d23ab8f136424b142bd7581ab805a0543a6
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-10-02 19:48:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from pymongo.mongo_client import MongoClient
 import json
 import os
+import time
 conp = MongoClient("mongodb+srv://thejatin:jatinkalwar@attacknum.nmuaiq8.mongodb.net/?retryWrites=true&w=majority")
+while True:
+    for x in conp.SumayaAcademyapk.youtubeurl.find():
+        urll = conp.SumayaAcademyapk.youtubeurl.find_one({"sno": x['sno']}, {'url': 1 , '_id': 0})
+        oldurl = urll['url']
 
-for x in conp.SumayaAcademyapk.youtubeurl.find():
-    urll = conp.SumayaAcademyapk.youtubeurl.find_one({"sno": x['sno']}, {'url': 1 , '_id': 0})
-    oldurl = urll['url']
-
-    os.system(f"yt-dlp --get-url -f 22 {oldurl} >> vurl.txt")
-    with open('vurl.txt') as f:
-        newurl = f.read()
-    os.system("rm vurl.txt")
-    conp.attack.sumayaacademy.update_one({"sr": x['sno']}, {'$set': {"vurl": newurl}})
+        os.system(f"yt-dlp --get-url -f 18 {oldurl} >> vurl.txt")
+        with open('vurl.txt') as f:
+            newurl = f.read()
+        os.system("rm vurl.txt")
+        conp.attack.sumayaacademy.update_one({"sr": x['sno']}, {'$set': {"vurl": newurl}})
+    time.sleep(10)
