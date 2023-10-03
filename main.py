79a8c73367c2f2f82cf65c38b6b4a201bbd4481d
thehestestex <138957007+thehestestex@users.noreply.github.com>
2023-10-03 19:37:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,55 +20,15 @@ conp = MongoClient("mongodb+srv://thejatin:jatinkalwar@attacknum.nmuaiq8.mongodb
 kalwar = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 
-def statt():
-    def videourl():
-        while True:
-            print("Upon here")
-            for x in conp.SumayaAcademyapk.youtubeurl.find():
-                print("here")
-                urll = conp.SumayaAcademyapk.youtubeurl.find_one({"sno": x['sno']}, {'url': 1 , '_id': 0})
-                oldurl = urll['url']
-                os.system(f"yt-dlp --get-url -f 18 {oldurl} >> vurl.txt")
-                with open('vurl.txt') as f:
-                    newurl = f.read()
+def videourl():
+    while True:
+        for x in conp.SumayaAcademyapk.youtubeurl.find():
+            print("here")
+            urll = conp.SumayaAcademyapk.youtubeurl.find_one({"sno": x['sno']}, {'url': 1 , '_id': 0})
+            oldurl = urll['url']
+            os.system(f"yt-dlp --get-url -f 18 {oldurl} >> vurl.txt")
+            with open('vurl.txt') as f:
+                newurl = f.read()
                 os.system("rm vurl.txt")
                 conp.attack.sumayaacademy.update_one({"sr": x['sno']}, {'$set': {"vurl": newurl}})
             print("done")
-            time.sleep(18000)
-            print("start Again")
-
-    def startserver():
-        while True:
-            print("Sumaya Server Start")
-            requests.get("https://sumayaacademy.onrender.com/")
-            time.sleep(780)
-    def video():
-        while True:
-            requests.get("https://autourl-3ptn.onrender.com/")
-            time.sleep(300)
-
-    def printt():
-        while True:
-            print("working")
-            time.sleep(5)
-            
-            
-            
-
-
-
-
-    t1 = threading.Thread(target=videourl)
-    t2 = threading.Thread(target=startserver)
-    
-    t3 = threading.Thread(target=video)
-    t4 = threading.Thread(target=printt)
-
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-@kalwar.get("/", response_class=PlainTextResponse)
-async def verif(background_tasks: BackgroundTasks):
-   background_tasks.add_task(statt)
-   return "ok"
